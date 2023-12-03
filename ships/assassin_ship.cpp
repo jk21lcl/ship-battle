d2cd388514c23edb86bf4f3945241bfd2752d4f7
jk21lcl <748630503@qq.com>
2023-12-03 17:22:27 +0800

    inhance Assassin Ship
    加强刺客船

--- ships/assassin_ship.cpp
+++ ships/assassin_ship.cpp
@@ -5,6 +5,7 @@ AssassinShip::AssassinShip(Game* game, int id) : Ship(game, id)
 {
     health_ = 12;
     max_health_ = 12;
+    can_stunned_ = false;
     ship_type_ = assassin_ship;
     name_ = "Assassin Ship";
     cannons_.push_back(new Cannon1(game));
@@ -22,17 +23,23 @@ void AssassinShip::Update()
         skills_[0]->SetActive(true);
         skills_[1]->SetActive(false);
         skills_[2]->SetActive(false);
+        ratio_damage_reduce_ = 20;
+        absolute_damage_reduce_ = 0;
     }
     else if (health_ >= 5)
     {
         skills_[0]->SetActive(false);
         skills_[1]->SetActive(true);
         skills_[2]->SetActive(false);
+        ratio_damage_reduce_ = 20;
+        absolute_damage_reduce_ = 0;
     }
     else
     {
         skills_[0]->SetActive(false);
         skills_[1]->SetActive(false);
         skills_[2]->SetActive(true);
+        ratio_damage_reduce_ = 0;
+        absolute_damage_reduce_ = 1;
     }
 }
